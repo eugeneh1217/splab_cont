24f650ccad57ba70e2ef267e5c5cd435f9490a00
Andrew Hong <honga010807@gmail.com>
2025-03-29 22:25:13 -0400

    make splab page

--- frontend/src/integrals/Home.jsx
+++ frontend/src/integrals/Home.jsx
@@ -1,8 +1,19 @@
 
 function Home() {
   return (
-    <div>
-      hi
+    <div className="flex flex-col items-center text-3xl font-bold">
+      <h1 className="mt-[10%]">Create a Splab!</h1>
+      <form className="mt-[5%] flex flex-col gap-5">
+        <label className="text-sm font-normal">
+          Name:
+          <input className="border border-gray-300 rounded-md p-2" type="text"/>
+        </label>
+        <label className="text-sm font-normal">
+          Zelle ID:
+          <input className="border border-gray-300 rounded-md p-2" name="payment"/>
+        </label>
+        <input className="border broder-gray-300 round-md p-2" type="submit" value="Submit!"/>
+      </form>
     </div>
   )
 }
