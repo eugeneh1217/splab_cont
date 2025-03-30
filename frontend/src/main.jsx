a467daefcbbdafaeb1b45c4c01d536e58db92298
arnavaggarwal75 <arnavaggarwal907@gmail.com>
2025-03-30 00:39:18 -0400

    fixed usercontext bug

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { UserProvider } from "./contexts/UserContext.js";
+import { UserProvider } from "./contexts/UserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
