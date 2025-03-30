fd509a2210fcd3924d5839d4224fab699abe97fa
arnavaggarwal75 <arnavaggarwal907@gmail.com>
2025-03-30 00:35:40 -0400

    user context set up

---
+++ frontend/src/contexts/UserContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useContext } from "react";
+
+const UserContext = createContext();
+
+export function UserProvider({ children }) {
+  const [user, setUser] = useState({
+    name: "",
+    zelleId: "",
+    isOwner: false,
+  });
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export function useUser() {
+  return useContext(UserContext);
+}
