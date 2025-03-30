e52b3f6baa1c2d2d111b18c4827f435ba24d2378
arnavaggarwal75 <arnavaggarwal907@gmail.com>
2025-03-29 22:51:01 -0400

    upload page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./style.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./integrals/Home";
 import MemberFInal from "./integrals/MemberFInal";
+import Upload from "./integrals/Upload";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/member_final" element={<MemberFInal />} />
+          <Route path="/upload" element={<Upload />} />
         </Routes>
       </BrowserRouter>
     </div>
