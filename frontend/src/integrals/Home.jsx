07c5e0e05fd9c465164009bfb99b6b6c97adde98
Andrew Hong <honga010807@gmail.com>
2025-03-30 01:44:04 -0400

    added ability to join a splab from home page

--- frontend/src/integrals/Home.jsx
+++ frontend/src/integrals/Home.jsx
@@ -1,19 +1,48 @@
+import { useState } from "react";
 
 function Home() {
+
+  const [isOwner, setIsOwner] = useState(true);
+
   return (
     <div className="flex flex-col items-center text-3xl font-bold">
-      <h1 className="mt-[10%]">Create a Splab!</h1>
-      <form className="mt-[5%] flex flex-col gap-5">
-        <label className="text-sm font-normal">
-          Name:
-          <input className="border border-gray-300 rounded-md p-2" type="text"/>
-        </label>
-        <label className="text-sm font-normal">
-          Zelle ID:
-          <input className="border border-gray-300 rounded-md p-2" name="payment"/>
-        </label>
-        <input className="border broder-gray-300 round-md p-2" type="submit" value="Submit!"/>
-      </form>
+      {isOwner ? (
+        <div>
+          <h1 className="mt-[10%]">Create a Splab!</h1>
+          <form className="mt-[5%] flex flex-col gap-5">
+            <label className="text-sm font-normal">
+              Name:
+              <input className="border border-gray-300 rounded-md p-2" type="text"/>
+            </label>
+            <label className="text-sm font-normal">
+              Zelle ID:
+              <input className="border border-gray-300 rounded-md p-2" name="payment"/>
+            </label>
+            <input className="border broder-gray-300 round-md p-2" type="submit" value="Submit!"/>
+          </form>
+          <div className="w-full flex flex-col items-center">
+            <button className="text-sm font-normal" onClick={() => setIsOwner(false)}>Join a Splab?</button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1 className="mt-[10%]">Join a Splab!</h1>
+          <form className="mt-[5%] flex flex-col gap-5">
+            <label className="text-sm font-normal">
+              Name:
+              <input className="border border-gray-300 rounded-md p-2" type="text"/>
+            </label>
+            <label className="text-sm font-normal">
+              Code:
+              <input className="border border-gray-300 rounded-md p-2" name="payment"/>
+            </label>
+            <input className="border broder-gray-300 round-md p-2" type="submit" value="Submit!"/>
+          </form>
+          <div className="w-full flex flex-col items-center">
+            <button className="text-sm font-normal" onClick={() => setIsOwner(true)}>Create a Splab?</button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
